8abee4847b1236deadf0be0595fdfc302d2d8a68
Deepanshu Vangani <vanganideepanshu@gmail.com>
2021-12-08 19:14:53 +0530

    Update average.js

--- average.js
+++ average.js
@@ -1,6 +1,7 @@
 
 function average(numbers) {
-  return numbers.filter(c => !isNaN(c)).reduce((p, c)=> p + c, 0) / numbers.length;
+  return numbers.filter(c => !isNaN(c))
+    .reduce((p, c)=> p + c, 0) / numbers.length;
 }
 
 module.exports = {average};
